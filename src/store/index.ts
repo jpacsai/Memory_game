87069a0cfac0a68f079bf07e8ddacc76c25b4c99
unknown <judit.pacsai@gmail.com>
2019-03-12 23:42:20 +0100

    add basic styling

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import './index.scss'
 
 const extraArguments = { };
 export type ExtraArguments = typeof extraArguments;
