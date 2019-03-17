0f1890e22c055d9248d15856d3c76d03b500c738
unknown <judit.pacsai@gmail.com>
2019-03-17 15:32:35 +0100

    install redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import './index.scss'
@@ -6,7 +7,9 @@ import './index.scss'
 const extraArguments = { };
 export type ExtraArguments = typeof extraArguments;
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(extraArguments)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(extraArguments))));
 
 export type State = ReturnType<typeof rootReducer>;
 
