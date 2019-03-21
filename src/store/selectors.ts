e9437b007d566d30a6b1474f29545b14b1061fb4
unknown <judit.pacsai@gmail.com>
2019-03-22 00:04:23 +0100

    add  selectors

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,3 +2,7 @@ import { State } from './';
 import { createSelector } from 'reselect';
 
 export const getDeck = (state: State) => state.cards;
+
+export const getOpenCards = (state: State) => state.openCards;
+
+export const getMatchedCards = (state: State) => state.matchedCards;
