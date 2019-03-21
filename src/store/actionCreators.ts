c5aa4601235de6197841a214455b6ee2a42c7eea
unknown <judit.pacsai@gmail.com>
2019-03-22 00:03:06 +0100

    add actions

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -3,3 +3,7 @@ import * as actionNames from './actionNames';
 import { Card } from '../types';
 
 export const resolveCards = (cards: Card[]) => action(actionNames.RESOLVE_CARDS, cards);
+export const openCard = (card: number) => action(actionNames.OPEN_CARD, card);
+export const closeCards = () => action(actionNames.CLOSE_CARDS);
+export const matchedCards = (cards: number[]) => action(actionNames.MATCHED_CARDS, cards);
+export const clear = () => action(actionNames.CLEAR);
--- src/store/actionNames.ts
+++ src/store/actionNames.ts
@@ -1,1 +1,5 @@
 export const RESOLVE_CARDS = 'RESOLVE_CARDS';
+export const OPEN_CARD = 'OPEN_CARD';
+export const CLOSE_CARDS = 'CLOSE_CARDS';
+export const MATCHED_CARDS = 'MATCHED_CARDS';
+export const CLEAR = 'CLEAR';
