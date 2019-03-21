be86bcd5de5969b28bcc2e11b1ce55e3a6e61726
unknown <judit.pacsai@gmail.com>
2019-03-21 22:59:18 +0100

    add open/close click event to Cards

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Card as CardType } from '../types'
+import classnames from 'classnames';
+import { Card as CardType } from '../types';
 import './CardComponent.scss';
 
 export type CardComponentProps = {
   card: CardType;
+  open: boolean;
+  matched: boolean;
+  onClick: any;
 }
 
 class CardComponent extends React.PureComponent<CardComponentProps> {
+  handleClick = () => this.props.onClick(this.props.card.id);
+
   render() {
     const { card } = this.props;
     if (!card) return null;
     return (
-      <div className="CardComponent">
-        <img src={card.url} alt=""/>
+      <div className={"CardComponent"} >
+        <div
+          onClick={this.handleClick}
+          className={classnames('card-container', this.props.open ? 'open' : this.props.matched ? 'matched' : '')}
+         >
+          <img src={card.url} alt=""/>
+        </div>
       </div>
     );
   }
