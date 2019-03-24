b1d38e3280022cec468ff9c44c2aa60a65536a99
unknown <judit.pacsai@gmail.com>
2019-03-24 15:23:29 +0100

    add getMoves selector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -6,3 +6,5 @@ export const getDeck = (state: State) => state.deck;
 export const getOpenCards = (state: State) => state.openCards;
 
 export const getMatchedCards = (state: State) => state.matchedCards;
+
+export const getMoves = (state: State) => state.moves;
