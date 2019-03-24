90f4c09d8b10ea3fe0a8499ee55b4ef7fcafff46
unknown <judit.pacsai@gmail.com>
2019-03-24 15:46:46 +0100

    add dashboard buttons

--- src/components/DashboardButtons.tsx
+++ src/components/DashboardButtons.tsx
@@ -5,8 +5,12 @@ class DashboardButtons extends React.PureComponent {
   render() {
     return (
       <div className='DashboardButtons'>
-        <div>Pause</div>
-        <div>Restart</div>
+        <div className='pause-button'>
+          <i className="fa fa-pause"></i>
+        </div>
+        <div className='restart-button'>
+          <i className="fa fa-repeat"></i>
+        </div>
       </div>
     )
   }
