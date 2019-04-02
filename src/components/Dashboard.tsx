2bdc78cbecf55be5a58ec843fd538b401941435c
unknown <judit.pacsai@gmail.com>
2019-04-02 23:27:57 +0200

    fix Dashboard responsive layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import Timer from './Timer';
-import Moves from './Moves';
-import DashboardButtons from './DashboardButtons';
-import './Dashboard.scss';
+import Timer from "./Timer";
+import Moves from "./Moves";
+import DashboardButtons from "./DashboardButtons";
+import "./Dashboard.scss";
 
 class Dashboard extends React.PureComponent {
   render() {
     return (
       <div className="Dashboard">
-        <DashboardButtons />
-        <Timer />
-        <Moves />
+        <div className='dashboard-container'>
+          <DashboardButtons />
+          <Timer />
+          <Moves />
+        </div>
       </div>
     );
   }
