f791e2511d15f10ad187a59159fc0b811f71b7a4
unknown <judit.pacsai@gmail.com>
2019-04-04 23:29:49 +0200

    add timer selectors

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -8,3 +8,15 @@ export const getOpenCards = (state: State) => state.openCards;
 export const getMatchedCards = (state: State) => state.matchedCards;
 
 export const getMoves = (state: State) => state.moves;
+
+export const getTime = (state: State) => state.timer;
+
+export const getMinutes = createSelector(
+  getTime,
+  time => Math.floor(time / 60)
+);
+
+export const getSeconds = createSelector(
+  [getTime, getMinutes],
+  (time, minutes) => time - minutes * 60
+);
