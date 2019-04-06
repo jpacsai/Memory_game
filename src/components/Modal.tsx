2b082410496030ebc91034c246ec511a65159441
unknown <judit.pacsai@gmail.com>
2019-04-06 23:42:33 +0200

    add Modal base component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classnames from 'classnames';
+
+import './Modal.scss';
+
+export type ModalProps = {
+  isOpen?: boolean;
+  onClose?: Function;
+  modalClassName?: string;
+}
+
+class Modal extends React.PureComponent<ModalProps> {
+  static defaultProps = {
+    isOpen: false,
+    onClose: () => {}
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
+  };
+
+  handleClose = () => {
+    this.props.onClose!();
+  };
+
+  handleClick = (event:any) => {
+    if (this.refs.modal && !(this.refs.modal as Node).contains(event.target)) {
+      this.handleClose();
+    }
+  };
+
+  render() {
+    const { children, isOpen, modalClassName } = this.props;
+    if (!isOpen) return null;
+    const Modal = (
+      <div className="ModalContainer">
+        <div className={classnames("Modal", modalClassName)} ref="modal">
+          <main>{children}</main>
+        </div>
+      </div>
+    );
+    return ReactDOM.createPortal(Modal, document.getElementById('modal') as any);
+  }
+}
+
+export default Modal;
