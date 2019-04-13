1a3312a65288e5a1be5040c3511849ca56e68e45
unknown <judit.pacsai@gmail.com>
2019-04-13 16:25:40 +0200

    display stars based on scores value in state

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Stars.scss';
+import React from "react";
+import { connect } from "react-redux";
+import { State } from "../store";
+import { getScores } from "./../store/selectors";
+import "./Stars.scss";
 
-class Stars extends React.PureComponent {
+export type StarsProps = {
+  scores: number;
+};
+
+const mapStateToProps = (state: State) => ({
+  scores: getScores(state)
+});
+
+class Stars extends React.PureComponent<StarsProps> {
   render() {
+    const { scores } = this.props;
     return (
-      <div className='Stars'>⭐⭐⭐</div>
-    )
+      <div className="Stars">
+        {[...Array(scores)].map((e, i) => (
+          <span className="star" key={i}>⭐</span>
+        ))}
+      </div>
+    );
   }
 }
 
-export default Stars;
+export default connect(
+  mapStateToProps,
+  null
+)(Stars);
