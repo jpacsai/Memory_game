367c5cd2a20f8d9e1ad9a37bf8d3948c9d21e66a
unknown <judit.pacsai@gmail.com>
2019-04-25 23:51:20 +0200

    add getGameState selector, remove depricated selectors

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,6 +1,8 @@
 import { State } from './';
 import { createSelector } from 'reselect';
 
+export const getGameState = (state: State) => state.gameState;
+
 export const getDeck = (state: State) => state.deck;
 
 export const getOpenCards = (state: State) => state.openCards;
@@ -9,31 +11,16 @@ export const getMatchedCards = (state: State) => state.matchedCards;
 
 export const getMoves = (state: State) => state.moves;
 
+export const getScores = (state: State) => state.scores;
+
 export const getTimer = (state: State) => state.timer;
 
-export const getTimerValue = createSelector(
-  getTimer,
-  timer => timer.timerSeconds
-)
-
 export const getMinutes = createSelector(
-  getTimerValue,
+  getTimer,
   seconds => Math.floor(seconds / 60)
 );
 
 export const getSeconds = createSelector(
-  [getTimerValue, getMinutes],
+  [getTimer, getMinutes],
   (seconds, minutes) => seconds - minutes * 60
 );
-
-export const getTimerPaused = createSelector(
-  getTimer,
-  timer => timer.paused
-);
-
-export const getTimerOn = createSelector(
-  getTimer,
-  timer => timer.timerOn
-);
-
-export const getScores = (state: State) => state.scores;
