83d7bfaeff0129f0ec77c5acc979022694ab6765
unknown <judit.pacsai@gmail.com>
2019-04-25 23:50:37 +0200

    simplify timer reducer

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -9,9 +9,7 @@ export const resolveOpenCard = (card: number) => action(actionNames.OPEN_CARD, c
 export const closeOpenedCards = () => action(actionNames.CLOSE_CARDS);
 export const resolveMatchedCards = (cards: number[]) => action(actionNames.MATCHED_CARDS, cards);
 export const resolveMove = () => action(actionNames.RESOLVE_MOVE);
-export const startTime = () => action(actionNames.START_TIMER);
 export const resolveTime = () => action(actionNames.RESOLVE_TIME);
 export const resolvePause = (pause: boolean) => action(actionNames.RESOLVE_PAUSE, pause);
 export const deductScore = () => action(actionNames.DEDUCT_SCORE);
-export const endGame = () => action(actionNames.END_GAME);
 export const clear = () => action(actionNames.CLEAR);
--- src/store/reducers/timer.ts
+++ src/store/reducers/timer.ts
@@ -1,36 +1,12 @@
-import {
-  START_TIMER,
-  END_GAME,
-  RESOLVE_TIME,
-  RESOLVE_PAUSE,
-  CLEAR
-} from "../actionNames";
+import { RESOLVE_TIME, CLEAR } from "../actionNames";
 import { Actions } from "../actionTypes";
 
-export type Timer = {
-  timerSeconds: number;
-  timerOn: boolean;
-  paused: boolean;
-};
-
-const defaultState: Timer = {
-  timerSeconds: 0,
-  timerOn: false,
-  paused: false
-};
-
-export default (state: Timer = defaultState, action: Actions): Timer => {
+export default (state: number = 0, action: Actions): number => {
   switch (action.type) {
-    case START_TIMER:
-      return { ...state, timerOn: true };
     case RESOLVE_TIME:
-      return { ...state, timerSeconds: state.timerSeconds + 1 };
-    case RESOLVE_PAUSE:
-      return { ...state, paused: action.payload };
-    case END_GAME:
-      return { ...state, timerOn: false, paused: false };
+      return state + 1;
     case CLEAR:
-      return defaultState;
+      return 0;
     default:
       return state;
   }
