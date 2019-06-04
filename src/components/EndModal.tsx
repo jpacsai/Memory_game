0a2f61a137ef04d8cec49cce68cd0c4e0d13e19d
unknown <judit.pacsai@gmail.com>
2019-06-04 23:38:32 +0200

    add content to EndModal

--- src/components/EndModal.tsx
+++ src/components/EndModal.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import { State } from "../store";
-import { getMinutes, getSeconds, getMoves, getScores } from './../store/selectors';
-import Modal from './Modal';
+import {
+  getMinutes,
+  getSeconds,
+  getMoves,
+  getScores
+} from "./../store/selectors";
+import Modal from "./Modal";
+import Stars from "./Stars";
+
+import "./EndModal.scss";
 
 const mapStateToProps = (state: State) => ({
   min: getMinutes(state),
@@ -18,30 +26,37 @@ export type EndModalProps = {
   sec: number;
   moves: number;
   scores: number;
-}
+};
 
 class EndModal extends React.PureComponent<EndModalProps> {
   handleClose = () => {
     const { onClose } = this.props;
     if (onClose) onClose();
   };
-  
+
   render() {
     const { isOpen, min, sec, moves, scores } = this.props;
     if (!isOpen) return null;
-    const time = `your time: ${min ? min > 1 ? ' minutes' : ' minute' : ''} ${sec} ${sec > 1 ? 'seconds' : 'second'}`;
+    const time = `${min ? (min > 1 ? " minutes" : " minute") : ""} ${sec} ${
+      sec > 1 ? "seconds" : "second"
+    }`;
     return (
-      <Modal isOpen={isOpen} onClose={this.handleClose}>
-        <div>
-          {time}
+      <Modal
+        isOpen={isOpen}
+        onClose={this.handleClose}
+        title="Congratulations!"
+        modalClassName="EndModal"
+      >
+        <div className="info">
+          You won with <span>{moves} moves</span> in <span>{time}</span>
         </div>
-        <div>
-          {moves} moves
-        </div>
-        <div>{scores} scores</div>
+        <Stars />
       </Modal>
     );
   }
 }
 
-export default connect(mapStateToProps, null)(EndModal);
+export default connect(
+  mapStateToProps,
+  null
+)(EndModal);
