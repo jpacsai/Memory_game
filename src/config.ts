3eb6c0d3d8537715bc2ef0e83606633b8ceb8a4d
unknown <judit.pacsai@gmail.com>
2019-06-06 23:03:44 +0200

    add defaultDeck and space image urls to config

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,8 @@ export const deductScoreFirst = 12;
 
 export const deductScoreStep = 5;
 
+export const defaultDeck = "fruits";
+
 export const cardImages = [
   {
     name: "fruits",
@@ -18,6 +20,19 @@ export const cardImages = [
       "https://res.cloudinary.com/jutzee/image/upload/v1538133256/memory-game/banana.svg",
       "https://res.cloudinary.com/jutzee/image/upload/v1538133256/memory-game/cherry.svg",
       "https://res.cloudinary.com/jutzee/image/upload/v1538133256/memory-game/avocado.svg",
+    ]
+  },
+  {
+    name: "space",
+    images: [
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854133/memory-game/space/spaceship.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854132/memory-game/space/solar-system.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854131/memory-game/space/saturn.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854130/memory-game/space/planet-earth.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854129/memory-game/space/observatory.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854282/memory-game/space/comet-1.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854127/memory-game/space/venus.svg",
+      "https://res.cloudinary.com/jutzee/image/upload/v1559854126/memory-game/space/ursa-major.svg"
     ]
   }
 ] as Cards[];
