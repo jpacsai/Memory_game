4fc7e8a9d746ee131c08298fecf4b3eeca832c13
unknown <judit.pacsai@gmail.com>
2019-06-08 15:10:12 +0200

    add ThemeSelect to StartModal

--- src/components/StartModal.tsx
+++ src/components/StartModal.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { connect } from 'react-redux';
-import Select from 'react-select'
-import { cardImages } from '../config';
 import { createDeck } from "../store/actions";
 
 import Modal from "./Modal";
 import "./StartModal.scss";
+import ThemeSelect from "./ThemeSelect";
 
 export type StartModalProps = {
   isOpen: boolean;
@@ -14,11 +13,6 @@ export type StartModalProps = {
 };
 
 const mapDispatchToProps = { createDeck };
-
-const options = cardImages.map(image => ({
-  value: image.name,
-  label: image.name.charAt(0).toUpperCase() + image.name.slice(1)
-}));
 
 class StartModal extends React.PureComponent<StartModalProps> {
   handleClose = () => {
@@ -45,7 +39,7 @@ class StartModal extends React.PureComponent<StartModalProps> {
         </div>
         <div className="theme-selector-container">
           <span>Choose a theme</span>
-          <Select options={options} onChange={this.handleChange}/>
+          <ThemeSelect onChange={this.handleChange}/>
         </div>
       </Modal>
     );
