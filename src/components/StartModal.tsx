864f4b384650da19407dc946232626a0628be7c8
unknown <judit.pacsai@gmail.com>
2019-06-08 15:09:13 +0200

    remove close button from StartModal

--- src/components/StartModal.tsx
+++ src/components/StartModal.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { connect } from 'react-redux';
+import Select from 'react-select'
+import { cardImages } from '../config';
+import { createDeck } from "../store/actions";
+
 import Modal from "./Modal";
 import "./StartModal.scss";
 
 export type StartModalProps = {
   isOpen: boolean;
   onClose: Function;
+  createDeck: typeof createDeck;
 };
+
+const mapDispatchToProps = { createDeck };
+
+const options = cardImages.map(image => ({
+  value: image.name,
+  label: image.name.charAt(0).toUpperCase() + image.name.slice(1)
+}));
 
 class StartModal extends React.PureComponent<StartModalProps> {
   handleClose = () => {
@@ -13,22 +26,33 @@ class StartModal extends React.PureComponent<StartModalProps> {
     if (onClose) onClose();
   };
 
+  handleChange = (selectedOption: any) => {
+    this.props.createDeck(selectedOption.value);
+    this.handleClose();
+  }
+
   render() {
     const { isOpen } = this.props;
     if (!isOpen) return null;
     return (
       <Modal
         isOpen={isOpen}
-        onClose={this.handleClose}
         title="Start"
         modalClassName="StartModal"
       >
         <div className="text">
           Find all 8 pairs of cards in the least number of moves and time.
         </div>
+        <div className="theme-selector-container">
+          <span>Choose a theme</span>
+          <Select options={options} onChange={this.handleChange}/>
+        </div>
       </Modal>
     );
   }
 }
 
-export default StartModal;
+export default connect(
+  null,
+  mapDispatchToProps
+)(StartModal);
