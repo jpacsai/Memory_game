6d17a0ad4913d54a872a2cea9d076739ac17af0b
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2019-06-10 15:33:58 +0200

    set defaultValue from store in ThemeSelect

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -1,28 +1,40 @@
 import React, { Fragment } from "react";
+import { connect } from 'react-redux';
+import { State } from "../store";
 import Select from "react-select";
 import { themes, defaultDeck } from "../config";
+import { getTheme } from './../store/selectors';
 
 import ThemePreview from "./ThemePreview";
 
 export type ThemeSelectProps = {
   onChange: Function;
+  theme: string;
 };
 
 export type ThemeSelectState = {
   selectedOption: string;
 };
 
+const mapStateToProps = (state: State) => ({
+  theme: getTheme(state)
+});
+
 const options = themes.map(image => ({
   value: image.name,
   label: image.name.charAt(0).toUpperCase() + image.name.slice(1)
 }));
 
-const defaultValue = options.filter(option => option.value === defaultDeck);
 
 class ThemeSelect extends React.PureComponent<ThemeSelectProps, ThemeSelectState> {
   state = {
-    selectedOption: defaultDeck
+    selectedOption: this.props.theme
   }
+
+  getDefaultValue = () => {
+    const defaultValue = options.find(option => option.value === this.props.theme);
+    return defaultValue;
+  };
 
   handleChange = (selectedOption: any) => {
     this.setState({ selectedOption: selectedOption.value });
@@ -31,14 +43,16 @@ class ThemeSelect extends React.PureComponent<ThemeSelectProps, ThemeSelectState
 
   render() {
     const { selectedOption } = this.state;
-
     return (
       <Fragment>
-        <Select options={options} onChange={this.handleChange} defaultValue={defaultValue}/>
+        <Select options={options} onChange={this.handleChange} defaultValue={this.getDefaultValue()}/>
         <ThemePreview selectedOption={selectedOption}/>
       </Fragment>
     );
   }
 }
 
-export default ThemeSelect;
+export default connect(
+  mapStateToProps,
+  null
+)(ThemeSelect);
